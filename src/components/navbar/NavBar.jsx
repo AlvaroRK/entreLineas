a44b8158f903b40d06c8b2ccbe3576d74df43bdc
AlvaroRK <adriano.rk06@gmail.com>
2025-03-18 17:32:39 -0300

    1.3 Agregado estilo basico al nav

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import "./NavBar.css";
+
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return (
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="/catalog">Catalog</Link>
-        <Link to="/cart">Cart</Link>
-    </nav>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
 
-export default NavBar
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <h1 className="logo">MiApp</h1>
+        {!isDesktop && (
+          <button className="menu-button" onClick={() => setIsOpen(!isOpen)}>
+            <span className={isOpen ? "icon close" : "icon"}></span>
+          </button>
+        )}
+        <ul className={`menu ${isOpen || isDesktop ? "open" : ""}`}>
+          <Link className="listItem" to="/">
+            <span>Home</span>
+          </Link>
+          <Link className="listItem" to="/catalog">
+            Catalog
+          </Link>
+          <Link className="listItem" to="/cart">
+            Cart
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
